db383b4f8a72e991f12dafcc9c850e511a1a7d58
Larousse Isaac <larousseisaac@gmail.com>
2025-10-06 10:45:19 +0000

    15 inch pizza size

---
+++ Basic Maths/pizza_area.py
@@ -0,0 +1,11 @@
+from math import pi
+
+if __name__ == "__main__":
+    pizza_size = 15
+    number_of_pizzas = 2
+
+    pizza_area = pi * (pizza_size / 2) ** 2
+    total_area = pizza_area * number_of_pizzas
+
+    print(f"A {pizza_size}-inch pizza has an area of {pizza_area:.2f} square inches.")
+    print(f"So in total you have a total pizza area of {total_area:.2f} square inches.")
